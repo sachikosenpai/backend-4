6ebc236f9b5d49d95d61c7b2077760494f28534f
sachikosenpai <popoolafeyisayo5@gmail.com>
2022-04-05 23:12:14 +0100

    second commit

---
+++ feyicodes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace feyicodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {string myName = ("feyisayo popoola!");
+               Console.WriteLine(myName);
+            int num = (100);
+            Console.WriteLine(num);
+            float rate = (10.2f);
+            Console.WriteLine(rate);
+            decimal amount = (100.10M);
+            Console.WriteLine(amount);
+            long L = (45755452222222222L);
+            Console.WriteLine(L);
+            uint ui = (100u);
+            Console.WriteLine(ui);
+            ulong ul = (4575545222222222222ul);
+            Console.WriteLine(ul);
+            bool isValid = (true);
+            Console.WriteLine(isValid);
+            int num2 = (55);
+            Console.WriteLine(num2);
+            uint unit = (500u);
+            Console.WriteLine(unit);
+
+            //implicit and explicit
+             int i = (75);
+            long j = (i);
+            Console.WriteLine(i);
+            double d = (75.25);
+            int k;
+            k = (int)d;
+            Console.WriteLine(k); }
+    }
+}
+
+            
+        
+
+            
+
+    
+
